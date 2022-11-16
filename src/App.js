3696304f7c818a3e61ce1f6ef6f2503cca2832ba
aaixn <axnguyen246@gmail.com>
2022-11-16 06:30:01 -0800

    set wallpaper functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Wallpaper from './Components/Wallpaper/Wallpaper';
 import { Route, Routes } from 'react-router-dom';
@@ -6,13 +6,13 @@ import Home from './Components/Home/Home';
 
 function App() {
 
-
+  const [wallpaper, setWallpaper] = useState('https://i.imgur.com/UlPvCp3.png')
   
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage: `url(${wallpaper})`}}>
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/wallpapers' element = {<Wallpaper />}></Route>
+        <Route path='/wallpapers' element = {<Wallpaper setWallpaper={setWallpaper}/>}></Route>
       </Routes>
     </div>
   );
--- src/Components/Wallpaper/Wallpaper.js
+++ src/Components/Wallpaper/Wallpaper.js
@@ -3,14 +3,16 @@ import wallpapers from '../Wallpaper/Library'
 import '../Wallpaper/Wallpaper.css'
 import {Link} from 'react-router-dom'
 
-export default function Wallpaper() {
+export default function Wallpaper({setWallpaper}) {
   return (
     <div className='wallpaper-select'>
         <h1>Wallpaper Select</h1>
         <div className='wallpaper-list'>
             {wallpapers.map((item, key) => {
                 return(
-                    <img src={item} key={key} className='wallpaper'></img>
+                    <img src={item} key={key} className='wallpaper' onClick={() => {
+                      setWallpaper(item)
+                    }}></img>
                 )
             })}
         </div>
