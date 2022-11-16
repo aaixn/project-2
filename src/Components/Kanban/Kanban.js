2319972898522ad9bb62386aec10c0ebb8ffb180
aaixn <axnguyen246@gmail.com>
2022-11-16 06:21:27 -0800

    kanban functionality

--- src/Components/Kanban/Kanban.js
+++ src/Components/Kanban/Kanban.js
@@ -9,9 +9,11 @@ export default function Kanban() {
 
     const [kanban, setkanban] = useState({ 
         todo: ['po', 'goop', 'buh'],
-        inProg: [],
-        complete: [],
+        inProg: ['pung', 'bung'],
+        complete: ['gung', 'guh', 'puh'],
     })
+
+    const [moveItem, setMoveItem] = useState('')
 
     // kanban = {
     //     todo = {[]},
@@ -32,41 +34,84 @@ export default function Kanban() {
     //     inProg: ['bung', 'pung']
     // }))
 
-    // useEffect(() => {
-    //     localStorage.setItem("kanban", JSON.stringify({kanban}))
-    // }, [kanban])
+    useEffect(() => {
+        localStorage.setItem("kanban", JSON.stringify({kanban}))
+    }, [kanban])
+    
 
+    const moveToDo = () => {
+        setkanban(kanban => ({
+            ...kanban,
+            todo: [...kanban.todo, moveItem]
+        }))
+    }
 
+    const moveInProg = () => {
+        setkanban(kanban => ({
+            ...kanban,
+            inProg: [...kanban.inProg, moveItem]
+        }))
+    }
 
+    const moveComplete = () => {
+        setkanban(kanban => ({
+            ...kanban,
+            complete: [...kanban.complete, moveItem]
+        }))
+    }
 
   return (
     <div className='kanban'>
         <div className='chart'>
-            <div className='todo'>
-                <p>to do</p>
+            <div className='kanban-section'>
+                <h3>to do</h3>
                 {(kanban.todo.map((item, key) => {
-                    return <p key={key}>{item}</p>
+                    return <p className='kanban-item' key={key} onClick={() => setMoveItem(item)}>{item}</p>
                 }))}
-                <BsFillArrowRightCircleFill />
+                <BsFillArrowRightCircleFill onClick={()=> {
+                    moveInProg()
+                    setkanban(kanban => ({
+                        ...kanban,
+                        todo: kanban.todo.filter(item => item !== moveItem)
+                    }))
+                    }} cursor='pointer'/>
             </div>
-            <div className='in-progress'>
-                <p>in progress</p>
+            <div className='kanban-section'>
+                <h3>in progress</h3>
                 {(kanban.inProg.map((item, key) => {
-                    return <p key={key}>{item}</p>
+                    return <p className='kanban-item' key={key} onClick={() => setMoveItem(item)}>{item}</p>
                 }))}
-                <BsFillArrowLeftCircleFill />
-                <BsFillArrowRightCircleFill />
+                <BsFillArrowLeftCircleFill onClick={()=> {
+                    moveToDo()
+                    setkanban(kanban => ({
+                        ...kanban,
+                        inProg: kanban.inProg.filter(item => item !== moveItem)
+                    }))
+                    }} cursor='pointer'/>
+                <BsFillArrowRightCircleFill onClick={()=> {
+                    moveComplete()
+                    setkanban(kanban => ({
+                        ...kanban,
+                        inProg: kanban.inProg.filter(item => item !== moveItem)
+                    }))
+                    }} cursor='pointer'/>
             </div>
-            <div className='complete'>
-                <p>complete</p>
+            <div className='kanban-section'>
+                <h3>complete</h3>
                 {(kanban.complete.map((item, key) => {
-                    return <p key={key}>{item}</p>
+                    return <p className='kanban-item' key={key} onClick={() => setMoveItem(item)}>{item}</p>
                 }))}
-                <BsFillArrowLeftCircleFill />
+                <BsFillArrowLeftCircleFill onClick={()=> {
+                    moveInProg()
+                    setkanban(kanban => ({
+                        ...kanban,
+                        complete: kanban.complete.filter(item => item !== moveItem)
+                    }))
+                    }} cursor='pointer'/>
             </div>
         </div>
         <button onClick={() => {setAddToDo(true)}}>+</button>
-        <AddToDo addToDo={addToDo} setAddToDo = {setAddToDo}/>
+        {addToDo && <AddToDo addToDo={addToDo} setAddToDo = {setAddToDo}/>}
     </div>
   )
 }
