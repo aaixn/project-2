35b607a54066edf62dd77c704da177504b1c56b1
aaixn <axnguyen246@gmail.com>
2022-11-17 23:09:31 -0800

    fix kanban

--- src/Components/Kanban/Kanban.js
+++ src/Components/Kanban/Kanban.js
@@ -24,28 +24,6 @@ export default function Kanban() {
 
     const [moveItem, setMoveItem] = useState('')
 
-    // kanban = {
-    //     todo = {[]},
-    //     inprogress = {[]},
-    //     complete = {[]}
-    // }
-
-    // localStorage.setItem('kanban', JSON.stringify(kanban))
-
-    // localStorage.getItem()
-
-    // const inProgress = window.localStorage.getItem("kanban").inProgress
-
-    // setInprogres(..inProgress, )
-
-    // setkanban(kanban => ({
-    //     ...kanban,
-    //     inProg: ['bung', 'pung']
-    // }))
-
-    useEffect(() => {
-        localStorage.setItem("kanban", JSON.stringify({kanban}))
-    }, [kanban])
     
 
     const moveToDo = () => {
@@ -76,7 +54,7 @@ export default function Kanban() {
                 <div className='kanban-section'>
                     <h3>to do</h3>
                     {(kanban.todo.map((item, key) => {
-                        return <p className='kanban-item' key={key} onClick={() => setMoveItem(item)}>{item}</p>
+                        return <p className='kanban-item' key={key} onClick={() => {setMoveItem(item)}} style={{textDecoration: moveItem===item ? 'underline' : 'none'}}>{item}</p>
                     }))}
                     <div className='move-section'>
                         <BsFillPlusCircleFill className='add' style={moveStyle} onClick={() => setAddToDo(!addToDo)}/>
@@ -85,14 +63,16 @@ export default function Kanban() {
                             setkanban(kanban => ({
                                 ...kanban,
                                 todo: kanban.todo.filter(item => item !== moveItem)
-                            }))
+                            }
+                            ))
+                            setMoveItem('')
                         }}/>
                     </div>
                 </div>
                 <div className='kanban-section'>
                     <h3>in progress</h3>
                     {(kanban.inProg.map((item, key) => {
-                        return <p className='kanban-item' key={key} onClick={() => setMoveItem(item)}>{item}</p>
+                        return <p className='kanban-item' key={key} onClick={() => setMoveItem(item)} style={{textDecoration: moveItem===item ? 'underline' : 'none'}}>{item}</p>
                     }))}
                     <div className='move-section'>
                         <BsFillArrowLeftCircleFill className='move-button' style={moveStyle} onClick={()=> {
@@ -101,6 +81,7 @@ export default function Kanban() {
                                 ...kanban,
                                 inProg: kanban.inProg.filter(item => item !== moveItem)
                             }))
+                            setMoveItem('')
                             }}/>
                         <BsFillArrowRightCircleFill className='move-button' style={moveStyle} onClick={()=> {
                             moveComplete()
@@ -108,13 +89,14 @@ export default function Kanban() {
                                 ...kanban,
                                 inProg: kanban.inProg.filter(item => item !== moveItem)
                             }))
+                            setMoveItem('')
                         }}/>
                     </div>
                 </div>
                 <div className='kanban-section'>
                     <h3>complete</h3>
                     {(kanban.complete.map((item, key) => {
-                        return <p className='kanban-item' key={key} onClick={() => setMoveItem(item)}>{item}</p>
+                        return <p className='kanban-item' key={key} onClick={() => setMoveItem(item)} style={{textDecoration: moveItem===item ? 'underline' : 'none'}}>{item}</p>
                     }))}
                     <div className='move-section'>
                         <BsFillArrowLeftCircleFill className='move-button' style={moveStyle} onClick={()=> {
@@ -123,6 +105,7 @@ export default function Kanban() {
                                 ...kanban,
                                 complete: kanban.complete.filter(item => item !== moveItem)
                             }))
+                            setMoveItem('')
                         }}/>
                     </div>
                 </div>
